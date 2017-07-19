4db76f2e45e2950b0aff2578b6b02962c488e0e5
Seah Chin Ying <scy.6char@gmail.com>
2017-07-20 01:13:45 +0800

    utils.py / grequests : quick hack 1/x, explanation in comments

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+# so apparently we need these 4 lines to prevent some "Max recursion depth error" from grequests
+# see more at https://github.com/kennethreitz/grequests/issues/103 and https://github.com/gevent/gevent/issues/903
 from gevent import monkey
 def stub(*args, **kwargs):  # pylint: disable=unused-argument
     pass
