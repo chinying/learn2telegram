6c8eb05db4576c9df2ab9c5f43ee4629fe21d316
Seah Chin Ying <scy.6char@gmail.com>
2017-07-20 01:25:16 +0800

    so the url expander fails if there isn't a http|s in front of it

--- divers.py
+++ divers.py
@@ -2,11 +2,12 @@ import utils
 import grequests
 
 def long_url(url):
+    if not url.startswith('http'):
+        url = "http://" + url
     r = [grequests.get("http://x.datasig.io/short?url=" + url)]
     grequests.map(r, utils.exception_handler)
     res = r[0].response.json()["/short"]
     if "destination" in res.keys():
         return res["destination"]
     else:
-        print(res)
         return "error"
