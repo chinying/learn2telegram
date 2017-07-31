974efa1b45f3b1d01460e1ad1907c1c2a6ae54f8
Seah Chin Ying <scy.6char@gmail.com>
2017-08-01 02:20:02 +0800

    added utils function for flags extraction from chat commands

--- utils.py
+++ utils.py
@@ -1,2 +1,9 @@
+import re
+
 def exception_handler(request, exception):
     print("Request failed")
+
+def extract_flags(text):
+    pat = re.compile("\ -\w+")
+    matches = re.findall(pat, text)
+    return matches
