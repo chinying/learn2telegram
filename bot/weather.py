fcb6cecff22f6cc339dc135abd57c86986d7a0d0
Seah Chin Ying <scy.6char@gmail.com>
2017-10-05 23:23:49 +0800

    weather with caching (untested)

--- bot/weather.py
+++ bot/weather.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import bot.utils as utils
 import bot.greq_pre
 import grequests
@@ -7,15 +8,25 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 DARKSKY_KEY = os.environ.get("DARKSKY_KEY")
 
-latlng = "1.3294,103.8021"
+latlng = "1.3294,103.8021" # TODO: fetch location from telegram, or maybe store as user settings in db
 settings = "units=uk2"
-last_checked = None # TODO
+last_checked = datetime.datetime.now()
+cached_val = None
 
-def fetch_weather(params=["Currently"]):
+def fetch_weather(params=["Currently"], flush=False):
+    global cached_val, last_checked
     # test to parse params
-    url = "https://api.darksky.net/forecast/{}/{}?{}".format(DARKSKY_KEY, latlng, settings)
-    r = [grequests.get(url)]
-    grequests.map(r, utils.exception_handler)
-    res = r[0].response.json()
-    current_weather = res["currently"]
-    return "Temp.: {}\u00B0C\nFeels like: {}\u00B0C".format(current_weather["temperature"], current_weather["apparentTemperature"])
+    now = datetime.datetime.now()
+    ret = cached_val
+    if (cached_val == None or (now - last_checked) > datetime.timedelta(minutes=30)) or flush is True:
+        url = "https://api.darksky.net/forecast/{}/{}?{}".format(DARKSKY_KEY, latlng, settings)
+        r = [grequests.get(url)]
+        grequests.map(r, utils.exception_handler)
+        res = r[0].response.json()
+        ret = res["currently"]
+        cached_val = ret
+    else:
+        # TODO clear
+        print("using cached value")
+    return "{}\nTemp.: {}\u00B0C\nFeels like: {}\u00B0C".format(ret["summary"], 
+        ret["temperature"], ret["apparentTemperature"])
