08d006959169a883b67b7d1962011bdf9247c1bd
Seah Chin Ying <scy.6char@gmail.com>
2017-12-06 01:22:41 +0800

    blind push, what could go wrong?

--- bot/bus.py
+++ bot/bus.py
@@ -7,6 +7,7 @@ import grequests
 
 load_dotenv(find_dotenv())
 LTA_TOKEN = os.environ.get("LTA_KEY")
+BACKEND_URL = os.environ.get("BACKEND_SERVER_URL")
 NEXT_BUS = "NextBus"
 NEXT_BUS2 = "NextBus2"
 NEXT_BUS3 = "NextBus3"
@@ -36,17 +37,20 @@ def parse_request(req):
         return "unknown param"
 
 def fetch_buses(stop_id):
-    url = "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=" + stop_id
+    lta_url = "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=" + stop_id
     headers = {'AccountKey': LTA_TOKEN}
-    r = [grequests.get(url, headers=headers)]
+    backend_url = BACKEND_URL + "api/fetch_name?id=" + stop_id
+    r = [grequests.get(lta_url, headers=headers), grequests.get(backend_url)]
     grequests.map(r, utils.exception_handler)
     ret = []
     response = r[0].response.json()["Services"]
+    stop_name = r[1].response.json()["stops"]
+
     if len(response) == 0:
         return "invalid stop id"
     else:
         for bus in response:
-            formatted_string = "🚌 {}: {} {} {}  {} {} {}  {} {} {}".format(bus["ServiceNo"], 
+            formatted_string = "{}\n🚌 {}: {} {} {}  {} {} {}  {} {} {}".format(stop_name[stop_id], bus["ServiceNo"], 
             parse_time(bus[NEXT_BUS]["EstimatedArrival"]), utils.surround_brackets(bus[NEXT_BUS]["Type"]), load_to_icon(bus[NEXT_BUS]["Load"]),
             parse_time(bus[NEXT_BUS2]["EstimatedArrival"]), utils.surround_brackets(bus[NEXT_BUS2]["Type"]), load_to_icon(bus[NEXT_BUS2]["Load"]),
             parse_time(bus[NEXT_BUS3]["EstimatedArrival"]), utils.surround_brackets(bus[NEXT_BUS3]["Type"]), load_to_icon(bus[NEXT_BUS3]["Load"]))
